fca6f3ff5b8c10bd728ea3d8b59a30edf5027e92
Mr Sotyato <uviwe6663@gmail.com>
2024-07-16 10:06:47 +0200

    Add files via upload

---
+++ UserLogin.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package userlogin;
+import userlogin.userloginui.UserLoginUI;
+
+/**
+ *
+ * @author 27687
+ */
+public class UserLogin {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new UserLoginUI().setVisible(true);
+    }
+    
+}
